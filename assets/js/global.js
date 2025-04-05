78bf17f25bad375d06432487915319c4cd700beb
krishnaa6268 <krishnaguptamdz@gmail.com>
2025-04-05 19:30:29 +0530

    footer-update

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,1 +1,12 @@
 console.log("Hello Creazen Crafts...");
+// -----------------Loader---------------------
+// loader script
+window.addEventListener("load", () => {
+  const loader = document.querySelector(".loader-box");
+
+  loader.classList.add("loader-box-hidden");
+
+  loader.addEventListener("transitionend", () => {
+      document.body.removeChild(".loader-box")
+  })
+})
